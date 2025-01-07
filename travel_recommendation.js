40eab8af771175d866e46a44349558eb31ec1668
Brian Raamiireezz <Brian_Raamiireezz@hotmail.com>
2025-01-07 16:13:49 -0600

    Add search result mapping and functionality to search and clear buttons

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,3 +1,28 @@
+const introduction = document.getElementById("introduction");
+const search_form = document.getElementById("search_form");
+const search_results = document.getElementById("search-results");
+const not_found_feedback = search_results.querySelector("p");
+
+function hide_sections(...section_elements) {
+    for (const element of section_elements) {
+        element.setAttribute("aria-hidden", "true");
+
+        element
+            .classList
+            .add("not-visible");
+    }
+}
+
+function show_sections(...section_elements) {
+    for (const element of section_elements) {
+        element.setAttribute("aria-hidden", "false");
+
+        element
+            .classList
+            .remove("not-visible");
+    }
+}
+
 async function fetch_destination_data(keyword) {
     const response = await fetch('./travel_recommendation_api.json');
     const json_data = await response.json();
@@ -31,7 +56,7 @@ function get_related_keyword(term) {
     }
 }
 
-async function get_data(search_term){
+async function get_data(search_term) {
     /* Get keyword  */
     const search_keyword = get_related_keyword(search_term);
     console.log(search_keyword);
@@ -40,16 +65,73 @@ async function get_data(search_term){
     return await fetch_destination_data(search_keyword);
 }
 
-function show_data() {
-    
+function show_data(data) {
+    const get_template =
+        (image_name, destination_name, destination_description) => {
+            return (`
+                <li class="recommendation">
+                    <img 
+                        src="assets/destinations/${image_name}" 
+                        alt="${destination_name} image" 
+                        aria-hidden="true"
+                    >
+                    <p class="recommendation__name">
+                        ${destination_name}
+                    </p>
+                    <p class="recommendation__description">
+                        ${destination_description}
+                    </p>
+                </li>
+            `);
+        }
+
+    /* Hide introduction section and not found feedback*/
+    hide_sections(introduction, not_found_feedback);
+
+    /* Map search results */
+    let results = "";
+    for (const element of data) {
+
+        if (element["cities"]) {
+            for (const city of element["cities"]) {
+                results += get_template(
+                    city["imageUrl"],
+                    city["name"],
+                    city["description"],
+                );
+            }
+        }
+        else {
+            results += get_template(
+                element["imageUrl"],
+                element["name"],
+                element["description"],
+            );
+        }
+
+    }
+
+    search_results
+        .querySelector("ul")
+        .innerHTML = results;
+
+    show_sections(search_results);
 }
 
-const search_form = document.getElementById('search_form');
-const search_input = document.getElementById('search_input');
+function show_no_results() {
+    search_results
+        .querySelector("ul")
+        .innerHTML = "";
+
+    hide_sections(introduction);
+    show_sections(not_found_feedback, search_results);
+}
 
 search_form.onsubmit =
     async (event) => {
         event.preventDefault()
+
+        const search_input = document.getElementById('search_input');
 
         /* Get search term */
         const search_term = search_input.value.toLowerCase();
@@ -60,5 +142,18 @@ search_form.onsubmit =
         console.log(data);
 
         /* Show data*/
-        show_data();
+        if (data.length == 0) {
+            show_no_results();
+        }
+        else {
+            show_data(data);
+        }
+
+    }
+
+search_form.onreset =
+    () => {
+        /* Show introduction section */
+        hide_sections(search_results, not_found_feedback);
+        show_sections(introduction);
     }
